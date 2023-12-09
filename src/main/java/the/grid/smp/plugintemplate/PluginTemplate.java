53fdd3dd20f85e87bd2f8e6e2070f198bf9f8bca
Dr Theo <71725814+DrTheodor@users.noreply.github.com>
2023-12-09 13:58:34 +0300

    Initial commit

---
+++ src/main/java/the/grid/smp/plugintemplate/PluginTemplate.java
@@ -0,0 +1,16 @@
+package the.grid.smp.plugintemplate;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PluginTemplate extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}

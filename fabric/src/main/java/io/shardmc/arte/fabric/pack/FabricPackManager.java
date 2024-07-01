6c741cf94fe31fcbdcd1ebe7e24b5b1d295061f5
NikSne <commits@niksne.ru>
2024-07-01 06:42:48 +0300

    revert changes from 95c94386

--- fabric/src/main/java/io/shardmc/arte/fabric/pack/FabricPackManager.java
+++ fabric/src/main/java/io/shardmc/arte/fabric/pack/FabricPackManager.java
@@ -7,11 +7,9 @@ import io.shardmc.arte.common.Arte;
 import io.shardmc.arte.common.pack.manager.PackManager;
 import io.shardmc.arte.common.pack.meta.BuiltPack;
 
-import java.util.*;
+import java.util.Optional;
 
 public class FabricPackManager extends PackManager {
-
-    protected final Map<ServerPlayerEntity, Set<UUID>> map = new HashMap<>();
 
     public FabricPackManager(Arte arte) {
         super(arte);
@@ -21,36 +19,8 @@ public class FabricPackManager extends PackManager {
         this.arte.logger().info("Applying pack to player " + player.getName().getString());
         Text prompt = Text.of(this.arte.config().getPrompt());
 
-        this.applyEffects(player);
         for (BuiltPack pack : this.zipper.getPacks()) {
             player.networkHandler.sendPacket(new ResourcePackSendS2CPacket(pack.uuid(), pack.getAddress(this.server), pack.hash(), pack.force(), Optional.ofNullable(prompt)));
         }
-    }
-
-    protected void setStatus(ServerPlayerEntity player, UUID uuid, Status status) {
-        Set<UUID> set = this.map.computeIfAbsent(player, k -> new HashSet<>());
-
-        switch (status) {
-            case WORKING -> set.add(uuid);
-            case DONE -> set.remove(uuid);
-        }
-
-        if (set.isEmpty()) {
-            this.map.remove(player);
-            this.clearEffects(player);
-        }
-    }
-
-    protected void applyEffects(ServerPlayerEntity player) {
-        player.setInvulnerable(true);
-    }
-
-    protected void clearEffects(ServerPlayerEntity player) {
-        player.setInvulnerable(false);
-    }
-
-    protected enum Status {
-        WORKING,
-        DONE
     }
 }
